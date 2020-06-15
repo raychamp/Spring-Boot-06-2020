74924b33c5eb8399c395f40247cf129605da77ef
Raymond Champion <raymond.champion@att.net>
2020-06-15 17:35:14 -0500

    Initial commit
    initial commit

---
+++ springdatajpa/src/test/java/com/bharath/springdatajpa/SpringdatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharath.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
